a8c9f5739aff0f10b1b20933e864f649102cd80b
Thamaraimanalan M <devthamaraimanalan.m@gmail.com>
2026-01-23 00:02:19 +0530

    fix vercel deployment error

--- app/components/mdx.tsx
+++ app/components/mdx.tsx
@@ -1,5 +1,4 @@
 "use client";
-// @ts-nocheck
 import * as React from "react";
 import Image from "next/image";
 import Link from "next/link";
@@ -8,8 +7,11 @@ import { useMDXComponent } from "next-contentlayer/hooks";
 function clsx(...args: any) {
 	return args.filter(Boolean).join(" ");
 }
+
+type ComponentProps = React.HTMLAttributes<HTMLElement>;
+
 const components = {
-	h1: ({ className, ...props }) => (
+	h1: ({ className, ...props }: ComponentProps) => (
 		<h1
 			className={clsx(
 				"mt-2 scroll-m-20 text-4xl font-bold tracking-tight",
@@ -18,7 +20,7 @@ const components = {
 			{...props}
 		/>
 	),
-	h2: ({ className, ...props }) => (
+	h2: ({ className, ...props }: ComponentProps) => (
 		<h2
 			className={clsx(
 				"mt-10 scroll-m-20 border-b border-b-zinc-800 pb-1 text-3xl font-semibold tracking-tight first:mt-0",
@@ -27,7 +29,7 @@ const components = {
 			{...props}
 		/>
 	),
-	h3: ({ className, ...props }) => (
+	h3: ({ className, ...props }: ComponentProps) => (
 		<h3
 			className={clsx(
 				"mt-8 scroll-m-20 text-2xl font-semibold tracking-tight",
@@ -36,7 +38,7 @@ const components = {
 			{...props}
 		/>
 	),
-	h4: ({ className, ...props }) => (
+	h4: ({ className, ...props }: ComponentProps) => (
 		<h4
 			className={clsx(
 				"mt-8 scroll-m-20 text-xl font-semibold tracking-tight",
@@ -45,7 +47,7 @@ const components = {
 			{...props}
 		/>
 	),
-	h5: ({ className, ...props }) => (
+	h5: ({ className, ...props }: ComponentProps) => (
 		<h5
 			className={clsx(
 				"mt-8 scroll-m-20 text-lg font-semibold tracking-tight",
@@ -54,7 +56,7 @@ const components = {
 			{...props}
 		/>
 	),
-	h6: ({ className, ...props }) => (
+	h6: ({ className, ...props }: ComponentProps) => (
 		<h6
 			className={clsx(
 				"mt-8 scroll-m-20 text-base font-semibold tracking-tight",
@@ -63,7 +65,7 @@ const components = {
 			{...props}
 		/>
 	),
-	a: ({ className, ...props }) => (
+	a: ({ className, ...props }: ComponentProps) => (
 		<Link
 			className={clsx(
 				"font-medium text-zinc-900 underline underline-offset-4",
@@ -72,22 +74,22 @@ const components = {
 			{...props}
 		/>
 	),
-	p: ({ className, ...props }) => (
+	p: ({ className, ...props }: ComponentProps) => (
 		<p
 			className={clsx("leading-7 [&:not(:first-child)]:mt-6", className)}
 			{...props}
 		/>
 	),
-	ul: ({ className, ...props }) => (
+	ul: ({ className, ...props }: ComponentProps) => (
 		<ul className={clsx("my-6 ml-6 list-disc", className)} {...props} />
 	),
-	ol: ({ className, ...props }) => (
+	ol: ({ className, ...props }: ComponentProps) => (
 		<ol className={clsx("my-6 ml-6 list-decimal", className)} {...props} />
 	),
-	li: ({ className, ...props }) => (
+	li: ({ className, ...props }: ComponentProps) => (
 		<li className={clsx("mt-2", className)} {...props} />
 	),
-	blockquote: ({ className, ...props }) => (
+	blockquote: ({ className, ...props }: ComponentProps) => (
 		<blockquote
 			className={clsx(
 				"mt-6 border-l-2 border-zinc-300 pl-6 italic text-zinc-800 [&>*]:text-zinc-600",
@@ -108,7 +110,7 @@ const components = {
 			{...props}
 		/>
 	),
-	hr: ({ ...props }) => (
+	hr: ({ ...props }: ComponentProps) => (
 		<hr className="my-4 border-zinc-200 md:my-8" {...props} />
 	),
 	table: ({ className, ...props }: React.HTMLAttributes<HTMLTableElement>) => (
@@ -125,7 +127,7 @@ const components = {
 			{...props}
 		/>
 	),
-	th: ({ className, ...props }) => (
+	th: ({ className, ...props }: ComponentProps) => (
 		<th
 			className={clsx(
 				"border border-zinc-200 px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right",
@@ -134,7 +136,7 @@ const components = {
 			{...props}
 		/>
 	),
-	td: ({ className, ...props }) => (
+	td: ({ className, ...props }: ComponentProps) => (
 		<td
 			className={clsx(
 				"border border-zinc-200 px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right",
@@ -143,7 +145,7 @@ const components = {
 			{...props}
 		/>
 	),
-	pre: ({ className, ...props }) => (
+	pre: ({ className, ...props }: ComponentProps) => (
 		<pre
 			className={clsx(
 				"mt-6 mb-4 overflow-x-auto rounded-lg bg-zinc-900 py-4",
@@ -152,7 +154,7 @@ const components = {
 			{...props}
 		/>
 	),
-	code: ({ className, ...props }) => (
+	code: ({ className, ...props }: ComponentProps) => (
 		<code
 			className={clsx(
 				"relative rounded border bg-zinc-300 bg-opacity-25 py-[0.2rem] px-[0.3rem] font-mono text-sm text-zinc-600",
